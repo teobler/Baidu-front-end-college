70d0cd6b93d800ae084a0c32ff5da93d90e5aaf5
teobler <teobler@163.com>
2017-06-04 19:36:48 +0800

    深入理解DFS和BFS的实现

--- 斌斌学院/works_8/index.js
+++ 斌斌学院/works_8/index.js
@@ -1,6 +1,7 @@
-var front = document.getElementById("front");
-var mid = document.getElementById("mid");
-var hind = document.getElementById("hind");
+var DFS = document.getElementById("DFS");
+var BFS = document.getElementById("BFS");
+var keyword = document.getElementById("keyword");
+var search = document.getElementById("search");
 var _root = document.getElementById("root");
 var timer = 0;
 
@@ -8,127 +9,69 @@ var timer = 0;
 function travel(Node){//使用 temier += 500 使每个setTimeout能够依次执行
   setTimeout(function(){
     Node.classList.add("ing");
-  }, timer += 500);
+  }, timer += 300);
 
   setTimeout(function(){
     Node.classList.remove("ing");
-  }, timer += 500);
-}
-
-
-function Node(_div){
-  this.data = _div.textContent;
-  this.parent = null;
-  this.children = [];
-}
-
-function Tree(_div){
-  var node = new Node(_div);
-  this.root = _div;
-}
-
-var tree = new Tree(_root);
-
-
-
-
-
-front.onclick = function a(callback) {
-
-    // this is a recurse and immediately-invoking function
-    (function recurse(currentNode) {
-        // step 2
-
-        for (var i = 0, length = currentNode.children.length; i < length; i++) {
-            // step 3
-
-            recurse(currentNode.children[i]);
-            travel(currentNode);
-
-        }
-
-        // step4 回调
-        (currentNode);
-
-        // step 1
-    })(tree.root);
-
+  }, timer += 300);
 }
 
 
 
 
 
+DFS.onclick = function () {
+  timer = 0;
+  var Node = _root;
+  var Nodelist = [];
+  
+  (function DFS(Node, Nodelist){
+    if(Node){
+      travel(Node)
+      Nodelist.push(Node)
+      for(var i = 0; i < Node.children.length; i++){
+        DFS(Node.children[i], Nodelist)
+      }
+    }
+  })(DFS(Node, Nodelist));
+}
 
+BFS.onclick = function (){
+  timer = 0;
+  var Node = _root;
+  var Nodelist = [];
+  var i = 0;
 
+  (function BFS(Node, Nodelist){
+    if(Node){
+      travel(Node)
+      Nodelist.push(Node);
+      BFS(Node.nextElementSibling, Nodelist)
+      Node = Nodelist[i++];
+      BFS(Node.firstElementChild, Nodelist);
+    }
+  })(BFS(Node, Nodelist));
 
+}
 
+search.onclick = function(){
+  var key = keyword.value;
+  var flag = 0;
 
+  if(Node){
+    travel(Node)
+    if(Node.childNodes[0].nodeValue == key){
+      flag = 1;
+      Node.classList.add("ing");
 
+    }
+    Nodelist.push(Node);
+    BFS(Node.nextElementSibling, Nodelist)
+    Node = Nodelist[i++];
+    BFS(Node.firstElementChild, Nodelist);
+  }
 
+  if(flag ==1){
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// function travel(Node){//使用 temier += 500 使每个setTimeout能够依次执行
-//   setTimeout(function(){
-//     Node.classList.add("ing");
-//   }, timer += 500);
-//
-//   setTimeout(function(){
-//     Node.classList.remove("ing");
-//   }, timer += 500);
-// }
-//
-// function preOrder(Node){//先序遍历
-//   if(Node){
-//     travel(Node);
-//     preOrder(Node.children[0]);
-//     preOrder(Node.children[1]);
-//   }
-// }
-//
-// function midOrder(Node){//中序遍历
-//   if(Node){
-//     midOrder(Node.children[0]);
-//     travel(Node);
-//     midOrder(Node.children[1]);
-//   }
-// }
-//
-// function reaOrder(Node){//后序遍历
-//   if(Node){
-//     reaOrder(Node.children[0]);
-//     reaOrder(Node.children[1]);
-//     travel(Node);
-//   }
-// }
-//
-// front.onclick = function(){//每次点击初始化timer，否则第二次执行遍历，时间将延长很多
-//   timer = 0;
-//   preOrder(_root);
-// }
-//
-// mid.onclick = function(){
-//   timer = 0;
-//   midOrder(_root);
-// }
-//
-// hind.onclick = function(){
-//   timer = 0;
-//   reaOrder(_root);
-// }
+  }
+}
